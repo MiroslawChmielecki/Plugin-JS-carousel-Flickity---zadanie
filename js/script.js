840b9cf481f314340b88be58af23d81291501dec
Miroslaw Chmielecki <misiak19@op.pl>
2019-05-24 13:38:04 +0200

    Used framework mustache

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
+(function () {
+  var templateList = document.getElementById('template-product-list').innerHTML;
+  
+  var results = document.getElementById('result');
+
+  Mustache.parse(templateList);
+
+  var listItems = '';
+
+  for (var i = 0; i < productsData.length; i++) {
+    listItems += Mustache.render(templateList, productsData[i]);
+  }
+
+  results.insertAdjacentHTML('beforeend', listItems);
+
+})();
+
+
+
+
+
 var elem = document.querySelector('.main-carousel');
 var flkty = new Flickity( elem, {
   // options
@@ -14,8 +35,8 @@ var flkty = new Flickity( '.main-carousel', {
   // options
 });
 
-var button = document.querySelector('#buttonReset');
-button.addEventListener('click', function(){
+var buttonReset = document.querySelector('#buttonReset');
+buttonReset.addEventListener('click', function(){
     flkty.select(0)
 });
 
@@ -25,3 +46,6 @@ flkty.on( 'scroll', function( progress ) {
   progress = Math.max( 0, Math.min( 1, progress ) );
   progressBar.style.width = progress * 100 + '%';
 });
+
+
+
