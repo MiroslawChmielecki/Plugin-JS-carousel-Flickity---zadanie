4d8f50633958b7c5398a62a6db399db384918d14
Miroslaw Chmielecki <misiak19@op.pl>
2019-05-26 11:28:47 +0200

    Add corret files

--- js/script.js
+++ js/script.js
@@ -1,3 +1,40 @@
+'use strict';
+(function () {
+  var templateList = document.getElementById('template-product-list').innerHTML;
+  var results = document.getElementById('result');
+  Mustache.parse(templateList);
+  var listItems = '';
+  var coordinates = [];
+  
+  for (var i = 0; i < productsData.length; i++) {
+    listItems += Mustache.render(templateList, productsData[i]);
+    coordinates.push(productsData[i].coords);
+    }
+
+  results.insertAdjacentHTML('beforeend', listItems);
+
+  window.initMap = function () {
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 6, 
+      center: coordinates[0]
+    });
+    for(var m = 0; m < coordinates.length; m++){
+      new google.maps.Marker({
+      position: coordinates[m],
+      map: map,
+    });
+  }
+  
+  };
+  
+})();
+
+
+
+
+
+
+
 var elem = document.querySelector('.main-carousel');
 var flkty = new Flickity( elem, {
   // options
@@ -14,8 +51,8 @@ var flkty = new Flickity( '.main-carousel', {
   // options
 });
 
-var button = document.querySelector('#buttonReset');
-button.addEventListener('click', function(){
+var buttonReset = document.querySelector('#buttonReset');
+buttonReset.addEventListener('click', function(){
     flkty.select(0)
 });
 
